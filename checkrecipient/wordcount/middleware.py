329846ffcfdabe7ecaf8e0a50013738f81375d07
Davide <davide@davidevitelaru.com>
2017-06-17 17:02:20 +0100

    Implementead async task for writing to database

--- checkrecipient/wordcount/middleware.py
+++ checkrecipient/wordcount/middleware.py
@@ -1,13 +1,13 @@
 from datetime import datetime
+import threading, time
 
 class WordCounterMiddleware(object):
 
     def __init__(self, get_response):
         self.get_response = get_response
+        self.sessions = {}
 
     def __call__(self, request):
-
-        # request.session["last_activity"] = datetime.now()
 
         '''
         In-memory variable data store
@@ -19,12 +19,19 @@ class WordCounterMiddleware(object):
         if "counters" not in request.session:
             request.session["counters"] = {}
 
-        return self.get_response(request)
+        response = self.get_response(request)
 
-# Called during request:
-# process_request(request)
-# process_view(request, view_func, view_args, view_kwargs)
-# Called during response:
-# process_exception(request, exception) (only if the view raised an exception)
-# process_template_response(request, response) (only for template responses)
-# process_response(request, response)
+        req_time = datetime.now()
+        self.sessions[request.session.session_key] = req_time
+        threading.Thread(target=self.write_to_db, args=(req_time, request.session, )).start()
+
+        return response
+
+    def write_to_db(self, start_time, session):
+        time.sleep(10)
+
+        if self.sessions[session.session_key] == start_time:
+            print("One thread is working for" + str(session_key))
+
+            # Freeing up the memory
+            self.sessions.pop(session.session_key)
--- checkrecipient/wordcount/tests.py
+++ checkrecipient/wordcount/tests.py
@@ -19,3 +19,11 @@ class TestUploadEmails(TransactionTestCase):
         for upload in input_data["uploads"]:
             response = self.client.post(UPLOAD_EMAILS,content_type="application/json", data=json.dumps(upload))
             self.assertEqual(200, response.status_code, response.content)
+
+    def test_big_sample(self):
+        input_file = open('./wordcount/test_data/uploads.json', 'r')
+        input_data = json.loads(input_file.read())
+
+        for upload in input_data["uploads"]:
+            response = self.client.post(UPLOAD_EMAILS,content_type="application/json", data=json.dumps(upload))
+            self.assertEqual(200, response.status_code, response.content)
